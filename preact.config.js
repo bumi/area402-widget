2604df9827f439699733dae2ddb876a63bad13fe
Michael Bumann <hello@michaelbumann.com>
2020-11-15 23:46:02 +0100

    Disable babel in preact.config
    The emotion and cli update seem to break the config

--- preact.config.js
+++ preact.config.js
@@ -2,22 +2,21 @@ export default (config, env, helpers) => {
   delete config.entry.polyfills;
   config.output.filename = "[name].js";
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  let { rule } = helpers.getLoadersByName(config, "babel-loader")[0];
-  let babelConfig = rule.options;
+  // let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  // let { rule } = helpers.getLoadersByName(config, "babel-loader")[0];
+  // let babelConfig = rule.options;
 
-  babelConfig.presets.push("preact-widget-scripts/babel");
-  babelConfig.plugins.push([
-    "emotion",
-    {
-      sourceMap: true,
-      autoLabel: true,
-    },
-  ]);
+  // babelConfig.presets.push("preact-widget-scripts/babel");
 
-  console.log(JSON.stringify(babelConfig));
+  // babelConfig.plugins.push([
+  //   "@emotion",
+  //  {
+  //    sourceMap: true,
+  //   autoLabel: true,
+  //  },
+  // ]);
 
-  plugin.options.disable = true;
+  // plugin.options.disable = true;
 
   if (env.production) {
     config.output.libraryTarget = "umd";
