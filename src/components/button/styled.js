a53a44065dc68cf151510f3c6e50766633d1b65c
Anup <allamsetty.anup@gmail.com>
2020-11-23 01:10:25 +0000

    Fix button css

--- src/components/button/styled.js
+++ src/components/button/styled.js
@@ -3,13 +3,14 @@ import styled from "@emotion/styled";
 export const StyledButton = styled.button`
   background: #8c54ff;
   border: 0;
-  border-radius: 5px;
   color: #fff;
   cursor: pointer;
   font-size: 16px;
   font-weight: 600;
+  min-width: 200px;
   width: fit-content;
   padding: 10px 15px;
+  border-radius: 50px;
   transition: all 100ms ease-out;
 
   &:hover {
