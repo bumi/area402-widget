63b81e4bd8dd35d6cfe848301132ce815dddd486
Anup <allamsetty.anup@gmail.com>
2020-11-26 23:32:31 +0000

    Initial view of payment screen

---
+++ src/views/payment-screen/index.js
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+const PaymentScreen = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <div>payments screen WIP</div>
+    </div>
+  );
+};
+
+export default PaymentScreen;
