bfcad8f8b478b7c7179a3f6759d223c4c2b18170
Anup <allamsetty.anup@gmail.com>
2020-11-29 08:21:16 +0000

    Payment Screen UI

--- src/views/payment-screen/index.js
+++ src/views/payment-screen/index.js
@@ -1,46 +1,82 @@
 import { h } from "preact";
+import QRCode from "qrcode.react";
 
 import Title from "../../components/title";
 import Button from "../../components/button";
+import Copy from "../../components/icons/copy";
 import {
-  QRCode,
   Content,
   Subtitle,
   Container,
+  StyledButton,
+  ClipboardText,
   ContentWrapper,
   ClipboardWrapper,
   AmountPlaceholder,
 } from "./styled";
 
 const PaymentScreen = ({
-  onRequestClose,
   title,
-  onOpenWalletClick,
-  qr_code_png,
+  currency,
+  identifier,
+  onRequestClose,
+  selectedAmount,
+  payment_request,
 }) => {
+  const copyToClipboard = () => {
+    var copyText = document.getElementById("lightningUrl");
+
+    /* Select the text field */
+    copyText.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  };
+
   return (
     <Container>
       <Title title={title} onRequestClose={onRequestClose} />
 
       <ContentWrapper>
         <Subtitle>
-          Pay with lightning <AmountPlaceholder>20$</AmountPlaceholder>
+          Pay with lightning:{"  "}
+          <AmountPlaceholder>{`${selectedAmount}${currency}`}</AmountPlaceholder>
         </Subtitle>
 
         <Content>
-          <QRCode src={qr_code_png} />
+          <QRCode
+            size={90}
+            level={"L"}
+            renderAs={"svg"}
+            value={identifier}
+            bgColor={"#ffffff"}
+            fgColor={"#000000"}
+            includeMargin={false}
+          />
 
-          <ClipboardWrapper>hello world</ClipboardWrapper>
+          <ClipboardWrapper>
+            <ClipboardText
+              readOnly
+              type="text"
+              id="lightningUrl"
+              value={identifier}
+            />
+
+            <span onClick={copyToClipboard}>
+              <Copy />
+            </span>
+          </ClipboardWrapper>
         </Content>
       </ContentWrapper>
 
-      <Button buttonClick={onOpenWalletClick} btnText="Open Wallet" />
+      <StyledButton href={`lightning:${payment_request}`}>
+        Open Wallet
+      </StyledButton>
     </Container>
   );
 };
 
 PaymentScreen.defaultProps = {
-  onOpenWalletClick: () => {},
   onRequestClose: () => {},
 };
 
