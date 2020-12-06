97a3adea2937d6e15a65855147b163d9cb07d0b0
Anup <allamsetty.anup@gmail.com>
2020-12-06 20:40:11 +0000

    Use common styles for layout

--- src/views/payment-screen/styled.js
+++ src/views/payment-screen/styled.js
@@ -1,42 +1,11 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  box-sizing: border-box;
-`;
-
-export const ContentWrapper = styled.div`
-  display: flex;
-  flex: 1;
-  width: 100%;
-  padding: 10px;
-  align-items: center;
-  flex-direction: column;
-  box-sizing: border-box;
-  justify-content: center;
-`;
-
-export const Content = styled.div`
-  display: flex;
-  align-items: center;
-  box-sizing: border-box;
-  flex-direction: column;
-  justify-content: space-around;
-  flex: 1;
-  width: 100%;
-  font-size: 16px;
-  padding: 4px 4px 10px 4px;
-`;
-
-export const Subtitle = styled.h4`
-  font-size: 20px;
-  padding-top: 20px;
-  box-sizing: border-box;
-`;
+export {
+  Content,
+  Subtitle,
+  Container,
+  ContentWrapper,
+} from "../../utils/common-styles";
 
 export const AmountPlaceholder = styled.span`
   color: #4761fb;
@@ -45,7 +14,7 @@ export const AmountPlaceholder = styled.span`
 
 export const ClipboardWrapper = styled.div`
   display: flex;
-  margin-top: 6px;
+  margin-top: 10px;
   padding: 6px 16px;
   background: #ffffff;
   border-radius: 30px;
