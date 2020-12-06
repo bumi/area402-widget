ef1b186612933204fc7c1c8cee5fa5af0f44d762
Anup <allamsetty.anup@gmail.com>
2020-12-06 20:32:52 +0000

    Fix the seconds to minutes timer logic

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,12 @@
-export const secondsToHms = (time) => {
-  const timeInSeconds = Number(time);
-  const hours = Math.floor(timeInSeconds / 3600);
-  const minutes = Math.floor((timeInSeconds % 3600) / 60);
-  const seconds = Math.floor((timeInSeconds % 3600) % 60);
+export const secondsToHms = (secs) => {
+  let hours = Math.floor(secs / (60 * 60));
+
+  let divisor_for_minutes = secs % (60 * 60);
+  let minutes = Math.floor(divisor_for_minutes / 60);
+
+  let divisor_for_seconds = divisor_for_minutes % 60;
+  let seconds = Math.ceil(divisor_for_seconds);
+
   return {
     hours,
     minutes,
