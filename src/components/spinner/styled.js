3ae4c865f8e75dc1314fbb80abf6e55b3856de46
Anup <allamsetty.anup@gmail.com>
2020-12-17 20:34:32 +0000

    Make height,width and color of the spinner configurable

--- src/components/spinner/styled.js
+++ src/components/spinner/styled.js
@@ -12,18 +12,18 @@ const dualRing = keyframes`
 
 export const SpinnerWrapper = styled.div`
   display: inline-block;
-  width: 14px;
-  height: 14px;
+  width: ${(p) => `${p.width}px`};
+  height: ${(p) => `${p.height}px`};
 
   &::after {
     content: " ";
     display: block;
-    width: 12px;
-    height: 12px;
     margin: 0 auto;
     border-radius: 50%;
-    border: 1px solid #fff;
-    border-color: #fff transparent #fff transparent;
+    border: 1px solid ${(p) => p.color};
+    width: ${(p) => `${p.width - 2}px`};
+    height: ${(p) => `${p.height - 2}px`};
+    border-color: ${(p) => p.color} transparent ${(p) => p.color} transparent;
     animation: ${dualRing} 1.2s linear infinite;
   }
 `;
