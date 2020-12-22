59a069c31a225b4e5aeb6c19c6e662c791d0fa6e
Michael Bumann <hello@michaelbumann.com>
2020-12-22 23:13:39 +0100

    Cleanup

--- src/views/loading-screen/index.js
+++ src/views/loading-screen/index.js
@@ -4,17 +4,6 @@ import styled from "@emotion/styled";
 import Spinner from "../../components/spinner";
 import { Content, Container, ContentWrapper } from "../../utils/common-styles";
 
-const Subtitle = styled.p`
-  font-family: "Open Sans", "Lucida Sans", Helvetica, Arial, sans-serif;
-  font-size: 1.1rem;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 1.5;
-  margin-top: 16px;
-  letter-spacing: 0em;
-  text-align: center;
-`;
-
 const LoadingScreen = (props) => {
   return (
     <Container>
