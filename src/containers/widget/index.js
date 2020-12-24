cbb6b0ad4116e5a0d5a4f3e4432eb5c75fedb8d6
Anup <allamsetty.anup@gmail.com>
2020-12-24 00:26:34 +0000

    Use WidgetBase for extending component

--- src/containers/widget/index.js
+++ src/containers/widget/index.js
@@ -1,179 +1,22 @@
-import { h, Component } from "preact";
+import { h } from "preact";
 import { CacheProvider } from "@emotion/react";
 
 import Modal from "../../components/modal";
 import Button from "../../components/button";
 
-import InvoiceService from "../../services/invoice";
+import WidgetBase from "../base-class";
 
-import DonateScreen from "../../views/donate-screen";
-import WelcomeScreen from "../../views/welcome-screen";
-import PaymentScreen from "../../views/payment-screen";
-import ThankyouScreen from "../../views/thankyou-screen";
-import LoadingScreen from "../../views/loading-screen";
-
-import {
-  DEFAULT_API_BASE_URL,
-  DEFAULT_BASE_CLASSNAME,
-} from "../../utils/constants";
 import { StyledCache } from "../../utils/styled-cache";
 import { WidgetWrapper } from "../../utils/widget-wrapper";
+import { DEFAULT_BASE_CLASSNAME } from "../../utils/constants";
 
-// Merge various configuration objects. The last has the highest priority.
-// usage: getInitialState(this.props, {title: "Hallo"});
-function getInitialState() {
-  const initialState = Object.assign(Widget.defaultProps, ...arguments);
-  return {
-    ...initialState,
-    currentScreen: initialState.welcomeMessage
-      ? "welcome-screen"
-      : "donate-screen",
-  };
-}
-
-class Widget extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = getInitialState(props);
-    this.invoiceService = new InvoiceService({
-      apiToken: this.props.apiToken,
-      apiBaseUrl: this.props.apiBaseUrl,
-      paymentRequestRenderer: this.paymentRequestRenderer,
-    });
-  }
-
-  componentDidMount() {
-    return fetch(`${this.props.apiBaseUrl}/v1/configuration`, {
-      headers: { "Api-Token": this.props.apiToken },
-    }).then((response) => {
-      if (response.ok) {
-        response.json().then((config) => {
-          this.widgetConfig = config;
-          this.setDefaults();
-        });
-      } else {
-        this.setDefaults();
-      }
-    });
-  }
-
-  setDefaults = () => {
-    this.setState(getInitialState(this.props, this.widgetConfig));
-  };
-
-  closeModal = () => {
-    this.invoiceService.reset();
-    this.setDefaults();
-  };
-
-  openModal = () => {
-    this.setState({
-      isModalOpen: true,
-    });
-  };
-
-  requestPayment = (args) => {
-    const amountInCents = parseInt(args.amount, 10);
-
-    if (!amountInCents) {
-      console.log("Invalid amount");
-    } else {
-      this.setState(
-        {
-          isModalOpen: true,
-          currentScreen: "loading-screen",
-        },
-        () => {
-          this.requestPaymentHandler(amountInCents);
-        }
-      );
-    }
-  };
-
-  openWidget = (attributes) => {
-    this.setState({
-      ...attributes,
-      isModalOpen: true,
-    });
-  };
-
-  handleDonateClick = () => {
-    this.setState({
-      currentScreen: "donate-screen",
-    });
-  };
-
-  paymentRequestRenderer = (invoice) => {
-    this.setState({
-      invoiceDetails: invoice,
-      fetchingInvoiceState: false,
-      currentScreen: "payment-screen",
-    });
-  };
-
-  requestPaymentHandler = (amountInCents) => {
-    this.setState({
-      fetchingInvoiceState: true,
-    });
-
-    this.invoiceService
-      .requestPayment({ amount: amountInCents })
-      .then((response) => {
-        if (response.settled) {
-          this.setState({
-            invoiceDetails: null,
-            currentScreen: "thankyou-screen",
-          });
-        }
-      });
-  };
-
-  handleSubscribeClick = (value = "") => {
-    console.log("entered email: ", value);
-  };
-
-  renderModalContent = () => {
-    switch (this.state.currentScreen) {
-      case "welcome-screen":
-        return (
-          <WelcomeScreen
-            message={this.state.welcomeMessage}
-            onDonateClick={this.handleDonateClick}
-          />
-        );
-      case "donate-screen":
-        return (
-          <DonateScreen
-            currency={this.state.currency}
-            paymentOptions={this.state.paymentOptions}
-            isLoading={this.state.fetchingInvoiceState}
-            onNextClick={this.requestPaymentHandler}
-            customAmount={this.state.customAmount}
-          />
-        );
-      case "payment-screen":
-        return <PaymentScreen {...this.state.invoiceDetails} />;
-      case "thankyou-screen":
-        return (
-          <ThankyouScreen
-            thankyouMessage={this.state.thankyouMessage}
-            onSubscribeClick={this.handleSubscribeClick}
-            enableEmailSubscription={this.state.enableEmailSubscription}
-          />
-        );
-      case "loading-screen":
-        return (
-          <LoadingScreen height={40} width={40} color="var(--primary-color)" />
-        );
-      default:
-        null;
-    }
-  };
-
+class Widget extends WidgetBase {
   render({}, { title, logo, actionLabel, isModalOpen, styles }) {
     return (
-      <WidgetWrapper customStyles={styles} className={`${DEFAULT_BASE_CLASSNAME}-wrapper`}>
+      <WidgetWrapper
+        customStyles={styles}
+        className={`${DEFAULT_BASE_CLASSNAME}-wrapper`}
+      >
         <CacheProvider
           value={StyledCache("fourohtwo", `.${DEFAULT_BASE_CLASSNAME}`)}
         >
@@ -192,24 +35,6 @@ class Widget extends Component {
   }
 }
 
-Widget.defaultProps = {
-  actionLabel: "Donate",
-  imageSrc: "",
-  currency: "EUR",
-  title: "",
-  logo: "",
-  customAmount: true,
-  paymentOptions: { "1€": 1, "2€": 2, "5€": 5, "10€": 10 },
-  screenName: "loading-screen",
-  enableEmailSubscription: false,
-  apiBaseUrl: DEFAULT_API_BASE_URL,
-  apiToken: "",
-  welcomeMessage: "",
-  thankyouMessage: "Thank you!",
-  isModalOpen: false,
-  invoiceDetails: null,
-  fetchingInvoiceState: false,
-  styles: "",
-};
+Widget.defaultProps = WidgetBase.defaultProps;
 
 export default Widget;
