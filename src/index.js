bd33eefb249272d4c4eadc9d7891aeb04eac9c5d
Anup <allamsetty.anup@gmail.com>
2020-12-24 00:22:28 +0000

    Export inline-widget component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import habitat from "preact-habitat";
 
 import Widget from "./containers/widget";
 
+import InlineWidget from "./containers/inline-widget";
+
+export default InlineWidget;
+
 let _habitat = habitat(Widget);
 
 _habitat.render({
